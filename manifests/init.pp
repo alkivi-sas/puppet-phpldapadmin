926c29bb9dbe0efac7c1056b03deb4be3363ceaf
Alkivi <admin@alkivi.fr>
2014-09-29 10:09:29 +0200

    dont declare vhost in puppet module, need to be in another location

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,37 +5,37 @@ class phpldapadmin (
 
 ) {
 
-  if ! defined(Class['apache']) {
-    fail('You must include the apache base before using php ldap admin')
-  }
-  else
-  {
-    # declare vhost
-    apache::vhost { 'ldap':
-      priority         => '002',
-      servername       => "ldap.${domain_name}",
-      port             => '443',
-      ssl              => true,
-      ssl_cert         => '/home/alkivi/www/ssl/alkivi.crt',
-      ssl_key          => '/home/alkivi/www/ssl/alkivi.key',
-      docroot          => '/usr/share/phpldapadmin/htdocs',
-      logroot          => '/home/alkivi/www/log',
-      access_log       => false,
-      override         => ['All'],
-      error_log_syslog => 'syslog',
-      custom_fragment  => 'CustomLog /home/alkivi/www/log/apache_access.log combined
-      ',
-      directories      => [
-        { 
-        path           => '/usr/share/phpldapadmin/htdocs',
-        options        => 'ExecCGI FollowSymlinks',
-        allow_override => ['Limit','AuthConfig'],
-        order          => ['allow','deny'],
-        },
-        ],
-    }
-    
-  }
+  #if ! defined(Class['apache']) {
+  #  fail('You must include the apache base before using php ldap admin')
+  #}
+  #else
+  #{
+  #  # declare vhost
+  #  apache::vhost { 'ldap':
+  #    priority         => '002',
+  #    servername       => "ldap.${domain_name}",
+  #    port             => '443',
+  #    ssl              => true,
+  #    ssl_cert         => '/home/alkivi/www/ssl/alkivi.crt',
+  #    ssl_key          => '/home/alkivi/www/ssl/alkivi.key',
+  #    docroot          => '/usr/share/phpldapadmin/htdocs',
+  #    logroot          => '/home/alkivi/www/log',
+  #    access_log       => false,
+  #    override         => ['All'],
+  #    error_log_syslog => 'syslog',
+  #    custom_fragment  => 'CustomLog /home/alkivi/www/log/apache_access.log combined
+  #    ',
+  #    directories      => [
+  #      { 
+  #      path           => '/usr/share/phpldapadmin/htdocs',
+  #      options        => 'ExecCGI FollowSymlinks',
+  #      allow_override => ['Limit','AuthConfig'],
+  #      order          => ['allow','deny'],
+  #      },
+  #      ],
+  #  }
+  #  
+  #}
 
 
   # declare all parameterized classes
